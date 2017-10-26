e75a152d56d0bdfe5175a5cb94791d5ad94e26b6
EdwinSilvaDK <Edwin-ms-1995@hotmail.com>
2017-10-26 14:10:52 +0200

    made convert class customer

---
+++ DemoBLL/Converters/CustomerConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using CustomersystemBLL.BusinessObjects;
+using System.Linq;
+using CustomerSystemDAL.Entities;
+
+namespace CustomerSystemBLL.Converters
+{
+    public class CustomerConverter
+    {
+        public CustomerConverter()
+        {
+
+        }
+        internal Customer Convert(CustomerBO cust)
+        {
+            if (cust == null) { return null; }
+            return new Customer()
+            {
+                Id = cust.Id,
+                firstName = cust.firstName,
+                lastName = cust.lastName
+            };
+        }
+        internal CustomerBO convert(Customer cust)
+        {
+            if (cust == null) { return null; }
+            return new CustomerBO()
+            {
+                Id = cust.Id,
+                firstName = cust.firstName,
+                lastName = cust.lastName,
+
+            };
+        }
+    }
+}
